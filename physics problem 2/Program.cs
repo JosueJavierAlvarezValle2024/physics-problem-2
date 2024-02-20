7989ed9a325cafc998abc1c591867dc6cceed9e6
Jjav2 <Jjav2@HPCOMPUTER18>
2024-02-19 19:14:56 -0600

    Agregar archivos de proyecto.

---
+++ physics problem 2/Program.cs
@@ -0,0 +1,9 @@
+﻿using physics_problem_2;
+
+double mass = 15; // Expressed in kilograms
+double aceleration = 8; // It is expressed in meters per second squared
+
+double _force = force.Calculateforce(mass, aceleration);
+
+Console.WriteLine("The force required to move the object is: "+ _force);// This is where the strength is printed
+Console.ReadLine();
---
+++ physics problem 2/force.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace physics_problem_2
+{
+    public class force
+    {
+        public static double Calculateforce(double mass, double aceleration)
+        {
+            return mass * aceleration;
+        }
+    }
+}
